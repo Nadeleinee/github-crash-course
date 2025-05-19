c61cb7b736e3880a2a15a192e918c5166824a50e
Kiran Magar <kiranmgr145@gmail.com>
2025-05-19 15:42:56 +0545

    added isAbove18 fn

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const age = 22;
-const name= 'Kiran Magar'
+const name1= 'Kiran Magar'
 const dob = '03/11/2002'
 
 function sayName(){
-    console.log('My name is '+ name)
+    console.log('My name is '+ name1)
+}
+
+function isAbove18() {
+    return age >=18
 }
